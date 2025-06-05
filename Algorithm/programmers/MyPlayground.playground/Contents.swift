625d40398e955e46617889bf1e8451f1f1544ebb
Yoon-Mac <sryoowk@gmail.com>
2025-06-05 10:23:10 +0900

    mac 동기화

---
+++ Algorithm/programmers/MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+func solution(_ sizes:[[Int]]) -> Int {
+    
+    return 0
+}
+
+let sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
---
+++ Algorithm/programmers/programmers/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  programmers
+//
+//  Created by Yoon on 6/5/25.
+//
+
+import Foundation
+
+func solution(_ sizes:[[Int]]) -> Int {
+
+    return 0
+}
+let sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
+print(solution(sizes))
+//let sizes2 = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]
+//let sizes3 = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
