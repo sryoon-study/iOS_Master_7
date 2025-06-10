05be7b5894ef8c3c41166e21956703ff3aafc6cb
Yoon-Mac <sryoowk@gmail.com>
2025-06-10 21:14:39 +0900

    TIL DAY15

---
+++ Algorithm/programmers/programmers/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Foundation
+
+func solution(_ files:[String]) -> [String]{
+    return[]
+}
+
+let test1 = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
+
+let test2 =  ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]
