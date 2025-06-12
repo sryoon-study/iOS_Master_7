5fa6124cfb0ec56b905e86c7b17c0f93f38da9f4
Yoon-Mac <sryoowk@gmail.com>
2025-06-13 08:55:23 +0900

    알고리즘 / 강의파일 정리

--- Algorithm/programmers/programmers/MyPlayground.playground/Contents.swift
+++ Algorithm/programmers/programmers/MyPlayground.playground/Contents.swift
@@ -1,16 +1,15 @@
-import UIKit
 import Foundation
+import UIKit
 
-func solution(_ clothes:[[String]]) -> Int {
-    let categories = Set(clothes.map{ $0[1] })
+func solution(_ clothes: [[String]]) -> Int {
+    let categories = Set(clothes.map { $0[1] })
     var cntArr: [Int] = []
     for category in categories {
         var cnt = clothes.filter { $0[1] == category }.count
-        cnt+=1
+        cnt += 1
         cntArr.append(cnt)
     }
     return cntArr.reduce(1, *) - 1
-    
 }
 
 func solution2(_ clothes: [[String]]) -> Int {
@@ -22,31 +21,30 @@ func solution2(_ clothes: [[String]]) -> Int {
 
 let clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
 
-//let clothes = [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
+// let clothes = [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
 
 solution(clothes)
 
 let tmp = Dictionary(grouping: clothes, by: { $0[1] }).values.count
-    
 
 print(tmp)
 
 [
     [
-        ["blue_sunglasses", "eyewear"]
+        ["blue_sunglasses", "eyewear"],
     ],
     [
         ["yellow_hat", "headgear"],
-        ["green_turban", "headgear"]
-    ]
+        ["green_turban", "headgear"],
+    ],
 ]
 
 [
     [
         ["yellow_hat", "headgear"],
-        ["green_turban", "headgear"]
+        ["green_turban", "headgear"],
     ],
     [
-        ["blue_sunglasses", "eyewear"]
-    ]
+        ["blue_sunglasses", "eyewear"],
+    ],
 ]
