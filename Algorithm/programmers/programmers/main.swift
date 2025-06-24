6882b674249079473b45fa94297f4f2cec60381a
Yoon-Mac <sryoowk@gmail.com>
2025-06-24 16:17:46 +0900

    싱크

--- Algorithm/programmers/programmers/main.swift
+++ Algorithm/programmers/programmers/main.swift
@@ -7,36 +7,21 @@
 
 import Foundation
 
-import Foundation
-
-func solution(_ priorities: [Int], _ location: Int) -> Int {
-    var queue = priorities.enumerated().map { ($0, $1) }  // (index, priority)
-    var printOrder = 0
-
-    while !queue.isEmpty {
-        let current = queue.removeFirst()
-
-        // 더 중요한 문서가 있으면 다시 뒤로
-        if queue.contains(where: { $0.1 > current.1 }) {
-            queue.append(current)
-        } else {
-            printOrder += 1
-            if current.0 == location {
-                return printOrder
-            }
-        }
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    commands.map { (command) -> Int in
+        Array(array[(command[0] - 1)..<command[1]]).sorted()[command[2] - 1]
     }
-
-    return -1 // 도달하지 않음
 }
 
-let priorities = [2, 1, 3, 2]
-let location = 2
-// [2 3 0 1]
-print(solution(priorities, location))
+let array = [1, 5, 2, 6, 3, 7, 4]
+let commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
-//let priorites2 = [1, 1, 9, 1, 1, 1]
-//let location2 = 0
-// [ 2 3 4 5 0 1]
-//print(solution(priorites2, location2))
+print(solution(array, commands))
 
+
+func solution2(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    commands.map { command in
+        let (i, j, k) = (command[0], command[1], command[2])
+        return array[(i - 1)..<j].sorted()[k - 1]
+    }
+}
