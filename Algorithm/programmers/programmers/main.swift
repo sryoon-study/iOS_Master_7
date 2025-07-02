ec49b0f6ea22b59208f18962b61d71fe6a7d9b43
Yoon-Mac <sryoowk@gmail.com>
2025-07-02 21:03:47 +0900

    TIL DAY31

--- Algorithm/programmers/programmers/main.swift
+++ Algorithm/programmers/programmers/main.swift
@@ -1,31 +1,31 @@
 
 import Foundation
 
-func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    var result: [[Int]] = []
+func solution(_ friends:[String], _ gifts:[String]) -> Int {
     
-    // 세로행을 가로처럼 변형
-    let newArr2 = (0..<arr2[0].count).map { index in
-        arr2.map { $0[index]}
-    }
+    var inderdict: [String: Int]
+    var dict: [String: [String: Int]]
     
-    // 이 이후 잘못됨
-    let flat = arr1.flatMap { $0 }
+    let splitGift = gifts.map { $0.split(separator: " ") }
     
-    for i in flat {
-        var tmp: [Int] = []
-        for item in newArr2 {
-            let sum = item.reduce(0, +) * i
-            tmp.append(sum)
-        }
-        result.append(tmp)
-    }
-    return result
+    // 여기서부터 막힘
+    let tmp = friends.map { name in splitGift.map { $0[0] == name ? $0[1] : "" } }
+    print(tmp)
+    return 0
+    
 }
 
+let friends = ["muzi", "ryan", "frodo", "neo"]
+let gifts = ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"]
+
+//let tmp = "muzi frodo"
+//print(tmp.split(separator: " "))
 
 
-let arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
-let arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+print(solution(friends, gifts))
 
-print(solution(arr1, arr2))
+
+/*
+ gifts의 원소는 "A B"형태의 문자열입니다. A는 선물을 준 친구의 이름을 B는 선물을 받은 친구의 이름을 의미하며 공백 하나로 구분됩니다.
+ A와 B는 friends의 원소이며 A와 B가 같은 이름인 경우는 존재하지 않습니다.
+ */
